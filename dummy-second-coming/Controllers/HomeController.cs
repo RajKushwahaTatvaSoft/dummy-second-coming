f581889e8f1f6bd64597d19185875dd03a0d4542
Raj Kushwaha <raj.kushwaha@tatvasoft.com>
2025-01-10 16:09:40 +0530

    Add project files.

---
+++ dummy-second-coming/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using dummy_second_coming.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace dummy_second_coming.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
